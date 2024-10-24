c6ce853f0877270ea28bb7e405768047e1cf0cc0
Nash-Finkbeiner-Student <nafi36a@student.oaisd.org>
2024-10-24 12:43:29 +0000

    Finished All The Code

--- Program.cs
+++ Program.cs
@@ -10,8 +10,15 @@ public class Program
         Write1ThroughN_While(n);
         Write1ThroughN_For(n);
         WriteNThrough1_While(n);
+        WriteNThrough1_For(n);
+        WriteEvensThrough100();
+        FindSum(n);
+        FindSumOfEvenNumbers(n);
+        FindSumOfOddNumbers(n);
+        Console.WriteLine("Enter a number for a triangle base");
+        int _base = int.Parse(Console.ReadLine());
+        OutputRightTriangle(_base);
     }
-
     public static void Write1ThroughN_While(int n)
         {
             if(n <= 0)
@@ -31,7 +38,6 @@ public class Program
                 Console.WriteLine(list);
             }
         }
-
         public static void Write1ThroughN_For(int n)
         {
             if(n <= 0)
@@ -49,7 +55,6 @@ public class Program
              Console.WriteLine(list);
             }
         }
-
         public static void WriteNThrough1_While(int n)
         {
             if(n <= 0)
@@ -63,11 +68,127 @@ public class Program
              string list = "";
              while(n >= start)
                 {
-                list += n + " "; //Fix This Part
+                list += n + " "; 
                 n--;
                 }
                 Console.WriteLine(list);
             }
+        }
+        public static void WriteNThrough1_For(int n)
+        {
+            if(n <= 0)
+            {
+                Console.WriteLine(n + " is out of range");
+            }
+
+            else
+            {
+                string list = "";
+             for(int i = n; i >= 1; i--)
+             {
+                list += i + " ";
+             }
+             Console.WriteLine(list);
             }
         }
+        public static void WriteEvensThrough100()
+        {
+            int n = 0;
+            string list = "";
+            while(n <= 100)
+            {
+                n++;
+                if(n % 2 == 0)
+                {
+                    list += n + " ";
+                }
+            }
+            Console.WriteLine(list);
+        }
+        public static void FindSum(int n)
+        {
+             if(n <= 0)
+            {
+                Console.WriteLine(n + " is out of range");
+            }
+            else
+            {
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            }
+        }
+        public static void FindSumOfEvenNumbers(int n)
+        {
+            if(n <= 0)
+            {
+                Console.WriteLine(n + " is out of range");
+            }
+            else
+            {
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+            }
+        }
+        public static void FindSumOfOddNumbers(int n)
+        {
+             if(n <= 0)
+            {
+                Console.WriteLine(n + " is out of range");
+            }
+            else
+            {
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 2 != 0)
+                {
+                sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+            }
+        }
+        public static void OutputRightTriangle(int n)
+        {
+            if(n == 0)
+            {
+              Console.WriteLine(n + " is out of range");
+            }
+            else if(n < 0)
+            {
+                for(int i = n; i < 0; i++)
+                {
+                    string list = "";
+                    for(int j = -1; j >= i; j--)
+                    {
+                        list += "*";
+                    }
+                    Console.WriteLine(list);
+                }
+            }
+            else if(n > 0)
+            {
+                for(int i = 1; i <= n; i++)
+                {
+                    string list = "";
+                    for(int j = 1; j <= i; j++)
+                    {
+                        list += "*";
+                    }
+                    Console.WriteLine(list);
+                }
+            }
+        }
+        
 }
